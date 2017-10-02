15e88c9efb1e4a8b26c53ebd07db01086e3b429f
Kelly Fallon <kjfallon@syr.edu>
2017-10-02 10:49:54 -0400

    remove unused UserService, update usage example

--- src/main/java/edu/syr/cyberseed/sage/server/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package edu.syr.cyberseed.sage.server.services;
-
-import edu.syr.cyberseed.sage.server.entities.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByUsername(String username);
-}
