97c631401b0159505a86f75b653e59bab311869c
kjfallon <kjfallon@syr.edu>
2017-10-03 20:04:39 -0400

    add /viewRecord, include the patient in the list of viewers by default of a doctorExamRecord

---
+++ src/main/java/edu/syr/cyberseed/sage/server/configuration/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package edu.syr.cyberseed.sage.server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("edu.syr.cyberseed.sage.server.controllers.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Secure Medical Information Repository Kit (SMIRK) Documentation")
+                .description("This is dynamically generated documentation for SMIRK.  The web interface you are using right now may be used to both explore the API and " +
+                        "manually interact with the services granted to your authenticated user account.")
+                .termsOfServiceUrl("https://example")
+                .version("1.0")
+                .build();
+    }
+}
